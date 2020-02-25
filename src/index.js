10601e343c3c3fca52278d211f796c0bfb8ae175
Jagat Jeevan Sahoo <jagatsah@thoughtworks.com>
2020-02-25 14:40:15 +0530

    Add css variable for re-use

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
-import React, { useState } from "react";
-// import ReactDOM from "react-dom";
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 const App = ({ count: initialCount }) => {
-  const propsCount = parseInt(window.localStorage.getItem("count"));
-  const [count, setCount] = useState(initialCount || 0);
-  window.localStorage.removeItem("count");
-  return <button onClick={() => setCount(count + 1)}>{count}</button>;
+  const [photos, setPhoto] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/photos").then(response => {
+      response.json().then(data => setPhoto(data.slice(0, initialCount)));
+    });
+  }, []);
+
+  if (!photos.length) {
+    return "Photos still fetching";
+  }
+
+  const getPhotos = () => {
+    return photos.map(photo => {
+      return (
+        <a href={`/products/${photo.id}`} key={photo.id}>
+          <img src={photo.thumbnailUrl} alt={photo.title} id={photo.id} />
+        </a>
+      );
+    });
+  };
+
+  return (
+    <div className="page-view">
+      <h1>Products page</h1>
+      <div className="photo-container">{getPhotos()}</div>
+    </div>
+  );
 };
 
 export default App;
-// ReactDOM.render(<App />, document.querySelector("#products-page"));
